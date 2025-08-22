60305ad0b5fda09448b55cb06f7bb03f94fad048
MrClaro <adryan.contatoprofissional@gmail.com>
2025-08-21 21:16:08 -0300

    feat(app): adicionado rotas de books e counters

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: "books",
+		loadChildren: () =>
+			import("./components/books/books-module").then((m) => m.BooksModule),
+	},
+	{
+		path: "counters",
+		loadChildren: () =>
+			import("./components/counters/counters-module").then(
+				(m) => m.CountersModule,
+			),
+	},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
