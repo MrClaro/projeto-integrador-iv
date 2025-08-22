325a72cd216e4651022548fe85ac8c474d481dd0
MrClaro <adryan.contatoprofissional@gmail.com>
2025-08-21 21:18:14 -0300

    feat(component): criado component button em counters

---
+++ src/app/components/counters/button/button.ts
@@ -0,0 +1,21 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "app-button",
+	standalone: false,
+	templateUrl: "./button.html",
+	styleUrl: "./button.css",
+})
+export class Button {
+	count = 0;
+
+	onIncrement() {
+		this.count++;
+	}
+	onDecrement() {
+		if (this.count <= 0) {
+			return;
+		}
+		this.count--;
+	}
+}
